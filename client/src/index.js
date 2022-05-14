45315e450f9692d99cffa77ef5ba78959c88d2c5
Tony Rizzotto <tony@tonyrizzotto.com>
2022-05-14 09:46:24 -0400

    Adjust URI

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import App from './App'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4008',
+  uri: '/',
   cache: new InMemoryCache()
 });
 
